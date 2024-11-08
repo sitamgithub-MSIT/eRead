cbe124a299a428a20ebc8f02985ed7b93682c4a2
Sitam <sitammeur@gmail.com>
2024-11-08 22:54:47 +0530

    dot env setup ready

--- server.ts
+++ server.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import app from "./src/app";
+import { config } from "./src/config/config";
+
+const startServer = async () => {
+  const port = config.port || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+  });
+};
+
+startServer();
